f9fa5f04430e80e87e3a8f5528840796311a7578
ksparta <evakimathi@gmail.com>
2022-05-22 15:53:05 +0300

    Added authentication tokens

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,7 +10,10 @@ router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewset)
 
 
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
+
 ]
